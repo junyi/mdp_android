47e47974ece17616d304c9f3c9f923da779bf86a
Chua Yong Lun <c.yonglun@gmail.com>
2015-03-08 17:39:29 +0800

    changed direction buttons to image buttons

--- app/build/generated/source/r/debug/com/example/mdptool/R.java
+++ app/build/generated/source/r/debug/com/example/mdptool/R.java
@@ -10,37 +10,53 @@ package com.example.mdptool;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        public static final int dirButtonHeight=0x7f040000;
+        public static final int dirButtonWidth=0x7f040001;
+    }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
-        public static final int robert=0x7f020001;
+        public static final int down=0x7f020000;
+        public static final int downselector=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int left=0x7f020003;
+        public static final int leftselector=0x7f020004;
+        public static final int pressdown=0x7f020005;
+        public static final int pressleft=0x7f020006;
+        public static final int pressright=0x7f020007;
+        public static final int pressup=0x7f020008;
+        public static final int right=0x7f020009;
+        public static final int rightselector=0x7f02000a;
+        public static final int robert=0x7f02000b;
+        public static final int up=0x7f02000c;
+        public static final int upselector=0x7f02000d;
     }
     public static final class id {
-        public static final int action_connect=0x7f070019;
-        public static final int autoManual=0x7f070011;
-        public static final int button_conf=0x7f07000d;
-        public static final int button_down=0x7f070005;
-        public static final int button_f1=0x7f07000c;
-        public static final int button_f2=0x7f07000b;
-        public static final int button_left=0x7f070008;
-        public static final int button_right=0x7f070007;
-        public static final int button_scan=0x7f070004;
-        public static final int button_start=0x7f07000a;
-        public static final int button_stop=0x7f070009;
-        public static final int button_up=0x7f070006;
-        public static final int editText_F1=0x7f070015;
-        public static final int editText_F2=0x7f070016;
-        public static final int label=0x7f070018;
-        public static final int maze=0x7f07000e;
-        public static final int pairedDevice=0x7f070001;
-        public static final int receivedMsg=0x7f07000f;
-        public static final int save=0x7f070017;
-        public static final int scannedDevice=0x7f070003;
-        public static final int speechRec=0x7f070012;
-        public static final int textView1=0x7f070000;
-        public static final int textView2=0x7f070002;
-        public static final int toggleButton1=0x7f070013;
-        public static final int toggleButton2=0x7f070014;
-        public static final int toggleMap=0x7f070010;
+        public static final int action_connect=0x7f080019;
+        public static final int autoManual=0x7f080011;
+        public static final int button_conf=0x7f08000d;
+        public static final int button_down=0x7f080005;
+        public static final int button_f1=0x7f08000c;
+        public static final int button_f2=0x7f08000b;
+        public static final int button_left=0x7f080008;
+        public static final int button_right=0x7f080007;
+        public static final int button_scan=0x7f080004;
+        public static final int button_start=0x7f08000a;
+        public static final int button_stop=0x7f080009;
+        public static final int button_up=0x7f080006;
+        public static final int editText_F1=0x7f080015;
+        public static final int editText_F2=0x7f080016;
+        public static final int label=0x7f080018;
+        public static final int maze=0x7f08000e;
+        public static final int pairedDevice=0x7f080001;
+        public static final int receivedMsg=0x7f08000f;
+        public static final int save=0x7f080017;
+        public static final int scannedDevice=0x7f080003;
+        public static final int speechRec=0x7f080012;
+        public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080002;
+        public static final int toggleButton1=0x7f080013;
+        public static final int toggleButton2=0x7f080014;
+        public static final int toggleMap=0x7f080010;
     }
     public static final class layout {
         public static final int activity_bluetooth_service=0x7f030000;
@@ -49,14 +65,14 @@ public final class R {
         public static final int layout_listview=0x7f030003;
     }
     public static final class menu {
-        public static final int main_activity_actions=0x7f060000;
+        public static final int main_activity_actions=0x7f070000;
     }
     public static final class string {
-        public static final int action_connect=0x7f040000;
-        public static final int app_name=0x7f040001;
-        public static final int hello_world=0x7f040002;
-        public static final int title_activity_bluetooth_service=0x7f040003;
-        public static final int title_activity_preference_page=0x7f040004;
+        public static final int action_connect=0x7f050000;
+        public static final int app_name=0x7f050001;
+        public static final int hello_world=0x7f050002;
+        public static final int title_activity_bluetooth_service=0x7f050003;
+        public static final int title_activity_preference_page=0x7f050004;
     }
     public static final class style {
         /** 
@@ -67,9 +83,7 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
-        /**  All customizations that are NOT specific to a particular API-level can go here. 
-         */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppTheme=0x7f060001;
     }
 }
